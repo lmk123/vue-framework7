4b52d34c1855b552aa2f84c4b5d1f9e012c25fa7
lmk123 <me@limingkai.cn>
2016-05-25 12:55:54 +0800

    添加 openpack

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,10 +1,14 @@
 const webpack = require( 'webpack' ),
+  OpenPack = require( 'openpack' ),
   config = require( './webpack.base.config' );
 
 config.devtool = '#source-map';
 config.watch = true;
 config.plugins.push( new webpack.DefinePlugin( {
   'process.env.NODE_ENV': "'development'"
+} ) );
+config.plugins.push( new OpenPack( {
+  lan: true
 } ) );
 config.devServer = {
   host: '0.0.0.0',
