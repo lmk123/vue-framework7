416c0169094434d3dec608fc16b391f32165b2a1
lmk123 <me@limingkai.cn>
2016-06-06 02:20:37 +0800

    修复代码风格

--- src/_utils/transition-end.js
+++ src/_utils/transition-end.js
@@ -4,17 +4,17 @@
 let transitionEnd
 
 const { style } = document.createElement('div')
-  const transitions = {
-    'transition': 'transitionend',
-    'OTransition': 'otransitionend',
-    'MozTransition': 'transitionend',
-    'WebkitTransition': 'webkitTransitionEnd'
+const transitions = {
+  'transition': 'transitionend',
+  'OTransition': 'otransitionend',
+  'MozTransition': 'transitionend',
+  'WebkitTransition': 'webkitTransitionEnd'
+}
+for (let key in transitions) {
+  if (style[key] !== undefined) {
+    transitionEnd = transitions[key]
+    break
   }
-  for (let key in transitions) {
-    if (style[key] !== undefined) {
-      transitionEnd = transitions[key]
-      break
-    }
-  }
+}
 
 export default transitionEnd
