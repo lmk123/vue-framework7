333be22e6aad1e5b742bf6bdca10265bbef88e78
lmk123 <me@limingkai.cn>
2016-06-08 13:10:13 +0800

    发布新版本

--- build/webpack.combine.config.js
+++ build/webpack.combine.config.js
@@ -1,5 +1,6 @@
 const config = require('./webpack.build.config')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const webpack = require('webpack')
+const version = require('../package.json').version
 
 config.entry = './src/index'
 config.output.path = './'
@@ -7,6 +8,6 @@ config.output.filename = 'f7.js'
 config.output.library = 'F7'
 config.output.libraryTarget = 'umd'
 config.plugins.splice(0, 2)
-config.plugins.push(new ExtractTextPlugin('f7.css'))
+config.plugins.push(new webpack.BannerPlugin('f7.js v' + version + '\nhttps://github.com/lmk123/vue-framework7\nReleased under the MIT License.'))
 
 module.exports = config
