2f6a09efafd9b35c28d360dd28bfc36a74b04ceb
lmk123 <me@limingkai.cn>
2016-07-22 12:18:36 +0800

    修复报错

--- src/methods/login.js
+++ src/methods/login.js
@@ -1,5 +1,6 @@
 import f7 from '../f7'
 import mt from '../mixins/modal-transition'
+import noop from '../utils/noop'
 
 const { modal } = mt.transitions
 
@@ -7,7 +8,7 @@ f7.loginScreen = function (l = document.querySelector('.login-screen')) {
   modal.beforeEnter(l)
   // 触发重绘
   const { clientHeight } = l
-  modal.enter(l)
+  modal.enter(l, noop)
 }
 
 f7.closeLogin = function (l = document.querySelector('.login-screen')) {
